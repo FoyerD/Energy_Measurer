90e6dd8be978861da521edb791cab77f0125e8eb
FoyerD <amitfoye@post.bgu.ac.il>
2024-12-29 12:54:05 +0200

    added dual graph

--- mesure_dnc.py
+++ mesure_dnc.py
@@ -9,7 +9,7 @@ import json
 import numpy as np
 from DNC_mid_train.DNC_eckity_wrapper import DeepNeuralCrossoverConfig, GAIntegerStringVectorCreator, DeepNeuralCrossover
 from DNC_mid_train import dnc_runner_eckity
-from DNC_mid_train.multiparent_wrapper import BEFORE_GEN_EVENT_NAME, AFTER_GEN_EVENT_NAME
+from DNC_mid_train.multiparent_wrapper import BEFORE_TRAIN_EVENT_NAME, AFTER_TRAIN_EVENT_NAME 
 from Logger import Logger
 import sys
 from time import sleep
@@ -27,7 +27,7 @@ def setup_dnc_algo():
 
     ind_length = dataset_n_items
     min_bound, max_bound = 0, dataset_n_items - 1
-    population_size = 100
+    population_size = 1000
 
     individual_creator = GAIntegerStringVectorCreator(length=ind_length, bounds=(min_bound, max_bound))
     bpp_eval = dnc_runner_eckity.BinPackingEvaluator(n_items=dataset_n_items, item_weights=dataset_item_weights,
@@ -48,12 +48,15 @@ def setup_dnc_algo():
     dnc_op = DeepNeuralCrossover(probability=0.8, population_size=population_size, dnc_config=dnc_config,
                                  individual_evaluator=bpp_eval, vector_creator=individual_creator)
     
-    output_file = "./out_files/mesures/cpu_" + job_id + ".csv"
-    logger = Logger(job_id=job_id, output_file=output_file)
-    logger.setup_CPU()
-    logger.log_headers()
-    dnc_op.dnc_wrapper.register(BEFORE_GEN_EVENT_NAME, logger.log)
-    dnc_op.dnc_wrapper.register(AFTER_GEN_EVENT_NAME, logger.log)
+    logger_start = Logger(job_id=job_id, output_file="./out_files/mesures/cpu_" + job_id + ".csv")
+    logger_start.setup_CPU_before_train()
+    logger_start.log_headers()
+    dnc_op.dnc_wrapper.register(BEFORE_TRAIN_EVENT_NAME, logger_start.log)
+    
+    logger_fin = Logger(job_id=job_id, output_file="./out_files/mesures/cpu_" + job_id + ".csv")
+    logger_fin.setup_CPU_after_train()
+    dnc_op.dnc_wrapper.register(AFTER_TRAIN_EVENT_NAME, logger_fin.log)
+
 
     # Initialize the evolutionary algorithm
     return SimpleEvolution(
@@ -76,7 +79,7 @@ def setup_dnc_algo():
                       ),
         breeder=SimpleBreeder(),
         max_workers=1,
-        max_generation=10,
+        max_generation=50000,
         # termination_checker=ThresholdFromTargetTerminationChecker(optimal=100, threshold=0.0),
         statistics=BestAverageWorstStatistics(), random_seed=4242
     )
@@ -84,9 +87,8 @@ def setup_dnc_algo():
 
 def merge_csv():
     # Load the CSV files
-    cpu_data = pd.read_csv('./out_files/mesures/cpu_' + job_id + '.csv')  # Replace with your actual file name
-    gpu_data = pd.read_csv('./out_files/mesures/gpu_' + job_id + '.csv')  # Replace with your actual file name
-
+    cpu_data = pd.read_csv('./out_files/mesures/cpu_' + job_id + '.csv')  
+    gpu_data = pd.read_csv('./out_files/mesures/gpu_' + job_id + '.csv')  
     # Merge the two DataFrames
     merged_data = pd.concat([cpu_data, gpu_data])
 
@@ -98,28 +100,35 @@ def merge_csv():
     merged_data.to_csv('./out_files/mesures/merged_' + job_id + '.csv', index=False)
     return merged_data
     
-
-def plot_graph(merged_data):
+def plot_graphs(merged_data, statistics):
     # Replace GPU measure by the cumulative sum of GPU measures
     merged_data.loc[merged_data['type'] == 'GPU', 'measure'] = merged_data.loc[
         merged_data['type'] == 'GPU', 'measure'
     ].cumsum()   
 
+    fig, ax_left = plt.subplots()
     # Plot the data
     plt.figure(figsize=(12, 6))
     for data_type, group in merged_data.groupby('type'):
-        plt.plot(group['time'], group['measure'], label=data_type)
-        if(group['start_gen']): plt.axvline(x=group['time'])
+        ax_left.plot(group['time'], group['measure'], label=data_type)
+    for train_status, group in merged_data.groupby('train_status'):
+        if(train_status == 'Start'):
+            [ax_left.axvline(x=time, ymax=0.1, color='green') for time in group['time']]
+        elif(train_status == 'Finish'):
+            [ax_left.axvline(x=time, ymax=0.1, color='red') for time in group['time']]
+    ax_left.set_ylabel('mesures')
+    ax_left.set_xlabel('time')
 
+    ax_right = ax_left.twinx()
+    ax_right.plot(statistics['time'], statistics['best_of_gen'], label='best_of_gen', color='silver')
+    ax_right.plot(statistics['time'], statistics['best_of_run'], label='best_of_run', color='gold')
+    ax_right.plot(statistics['time'], statistics['average'], label='average', color='chocolate')
+    ax_right.set_ylabel('statistics')
 
-    # Add labels, title, and legend
-    plt.xlabel('Time')
-    plt.ylabel('Measure')
-    plt.title('Measure/Statistics vs Time')
-    plt.legend(title='Type')
-    plt.grid(True)
+    fig.suptitle('Measure/Statistics vs Time')
+    fig.legend()
+    fig.savefig('out_files/plots/plot_' + job_id + '.png', dpi=300, bbox_inches='tight')  # Adjust dpi for resolution
 
-    plt.savefig('out_files/plots/plot_' + job_id + '.png', dpi=300, bbox_inches='tight')  # Adjust dpi for resolution
 
 def main():
     assert (len(sys.argv) >= 1)
@@ -128,15 +137,20 @@ def main():
     sleep(sleep_time)
 
     algo = setup_dnc_algo()
-    statistics_logger = Logger(output_file="./out_files/statistics_" + job_id + ".csv", job_id=job_id)
+    statistics_logger = Logger(output_file="./out_files/statistics/statistics_" + job_id + ".csv" , job_id=job_id)
     statistics_logger.setup_evolution_statistics(algo)
+    statistics_logger.log_headers()
+    algo.register("after_generation", statistics_logger.log)
     # evolve the generated initial population
     algo.evolve()
     # Execute (show) the best solution
-    print(algo.execute())
+    algo.execute()
     prober.kill()
     merged_data = merge_csv()
-    plot_graph(merged_data)
+    statistics = pd.read_csv('./out_files/statistics/statistics_' + job_id + '.csv')
+    statistics['time'] = pd.to_datetime(statistics['time'])  # Ensure 'time' column is datetime
+    statistics = statistics.sort_values(by='time')
+    plot_graphs(merged_data, statistics)
 
 if __name__ == "__main__":
     job_id = str(sys.argv[1])
