3c0fdc67c11c558c54671f41f0284caa0edcc813
FoyerD <amitfoye@post.bgu.ac.il>
2025-04-22 16:55:15 +0300

    changed script to redirect output

--- scripts/measure.sh
+++ scripts/measure.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
-# Run the command with superuser privileges
-su -c 'pinpoint -c -r 2 -i 250 -e CPU,GPU -- python exp_runner.py k_point uniform bpp --n_gens 20'
+source out_files/exp_num
+OUT_DIR="out_files/exp_$EXP_NUM"
+mkdir -p $OUT_DIR
+pinpoint -c -r 1 -i 250 -e CPU,GPU -- python exp_runner.py k_point uniform bpp --n_gens 20 2> $OUT_DIR/raw.txt
+echo EXP_NUM=$((EXP_NUM + 1)) > out_files/exp_num
