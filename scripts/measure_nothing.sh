de14f78e140639be61d9b2a6330a285972f48fd8
FoyerD <amitfoye@post.bgu.ac.il>
2025-05-15 10:48:03 +0300

    intermidiet

---
+++ scripts/measure_nothing.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export PYTHONPATH="/home/debian/.local/lib/python3.9/site-packages:$PYTHONPATH"
+source out_files/exp_num
+python --version
+OUT_DIR="out_files/exp_$EXP_NUM"
+mkdir -p $OUT_DIR
+sudo chmod 777 $OUT_DIR
+OUT_FILE=$OUT_DIR/raw.txt
+
+pinpoint -c --timestamp -r 5 -i 250 -e rapl:pkg,GPU -o $OUT_FILE -- python nothing.py -o$OUT_DIR
+echo EXP_NUM=$((EXP_NUM + 1)) > out_files/exp_num
