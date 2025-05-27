39026a0ff5f088799e9a2f7d5fdc5ba003e0d55f
FoyerD <amitfoye@post.bgu.ac.il>
2025-05-27 19:29:29 +0300

    added script to parse baseline measure and return the mean pkg and mean gpu values

--- scripts/measure_nothing.sh
+++ scripts/measure_nothing.sh
@@ -8,4 +8,5 @@ sudo chmod 777 $OUT_DIR
 OUT_FILE=$OUT_DIR/raw.txt
 
 pinpoint -c --timestamp -r 5 -i 250 -e rapl:pkg,GPU -o $OUT_FILE -- python nothing.py -o$OUT_DIR -t10
+chmod 0777 $OUT_FILE
 echo EXP_NUM=$((EXP_NUM + 1)) > out_files/exp_num
--- scripts/parse.sh
+++ scripts/parse.sh
@@ -4,5 +4,11 @@
 python parse_pinpoint.py ./out_files/exp_$1/raw.txt ./out_files/exp_$1/parsed_measures
 python parse_statistics.py ./out_files/exp_$1/statistics.csv ./out_files/exp_$1/parsed_stats
 
+base_pkg=0
+base_gpu=0
+if [ -n "$2" ]; then
+    read base_pkg base_gpu < <(python parse_nothing.py ./out_files/exp_$2/raw.txt) 
+fi
+
 # Prepering the data for plotting
-python merge_files.py ./out_files/exp_$1/parsed_measures ./out_files/exp_$1/parsed_stats ./out_files/exp_$1 --sdatetime
+python merge_files.py ./out_files/exp_$1/parsed_measures ./out_files/exp_$1/parsed_stats ./out_files/exp_$1 --sdatetime --base_pkg $base_pkg --base_gpu $base_gpu
