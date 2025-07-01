4d04edf7a9f8be26714de41e34c1213f40e95b29
FoyerD <amitfoye@post.bgu.ac.il>
2025-07-01 20:39:16 +0300

    added memory no nothing

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -3,11 +3,11 @@
 min_gen=0
 max_gen=6000
 
-if [ $3 ]; then
-	min_gen=$3
+if [ $2 ]; then
+	min_gen=$2
 fi
-if [ $4 ]; then
-   	max_gen=$4
+if [ $3 ]; then
+   	max_gen=$3
 fi
 
-python plot.py ./imp_outs/exp_$1/mean_measures.csv ./imp_outs/exp_$1/mean_statistics.csv ./imp_outs/exp_$1/imgs $2 --min_gen $min_gen --max_gen $max_gen
+python plot.py ./imp_outs/exp_$1/mean_measures.csv ./imp_outs/exp_$1/mean_statistics.csv ./imp_outs/exp_$1/imgs --min_gen $min_gen --max_gen $max_gen $4
