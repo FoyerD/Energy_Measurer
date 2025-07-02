7ed83d7c4b442e6e32f3fba55f7d3532dc6a1abe
FoyerD <amitfoye@post.bgu.ac.il>
2025-07-02 07:50:14 +0000

    fixed script

--- scripts/measure.sh
+++ scripts/measure.sh
@@ -5,9 +5,9 @@ python --version
 OUT_DIR="out_files/exp_$EXP_NUM"
 echo EXP_NUM=$((EXP_NUM + 1)) > out_files/exp_num
 mkdir -p $OUT_DIR
-sudo chmod a+w, a+r $OUT_DIR
+sudo chmod a+w,a+r $OUT_DIR
 OUT_FILE=$OUT_DIR/raw.txt
 
 pinpoint -c --timestamp -r 5 -i 250 -e rapl:pkg,GPU -o $OUT_FILE -- python exp_runner.py dnc uniform bpp -n 6000 -stats -o$OUT_DIR
-chmod a+w, a+r $OUT_FILE
+chmod a+w,a+r $OUT_FILE
 
--- scripts/measure_nothing.sh
+++ scripts/measure_nothing.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-export PYTHONPATH="/home/debian/.local/lib/python3.9/site-packages:$PYTHONPATH"
+export PATH="/home/debian/anaconda3/envs/EM/bin/:/home/debian/repos/pinpoint/build:$PATH"  
 source out_files/exp_num
-python --version
 OUT_DIR="out_files/exp_$EXP_NUM"
 echo EXP_NUM=$((EXP_NUM + 1)) > out_files/exp_num
 mkdir -p $OUT_DIR
-sudo chmod a+w, a+r $OUT_DIR
+sudo chmod a+w,a+r $OUT_DIR
 OUT_FILE=$OUT_DIR/raw.txt
 
 pinpoint -c --timestamp -r 5 -i 250 -e rapl:pkg,GPU -o $OUT_FILE -- python nothing.py -o$OUT_DIR -t12
-chmod a+w, a+r $OUT_FILE
+chmod a+w,a+r $OUT_FILE
