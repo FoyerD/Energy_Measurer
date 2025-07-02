cf1c0f294dd7431f2b2361a202fb3a4b6b00b132
FoyerD <amitfoye@post.bgu.ac.il>
2025-07-02 07:27:41 +0000

    changed permissions to not allow execution (wtf)

--- scripts/measure_nothing.sh
+++ scripts/measure_nothing.sh
@@ -5,8 +5,8 @@ python --version
 OUT_DIR="out_files/exp_$EXP_NUM"
 echo EXP_NUM=$((EXP_NUM + 1)) > out_files/exp_num
 mkdir -p $OUT_DIR
-sudo chmod 777 $OUT_DIR
+sudo chmod a+w, a+r $OUT_DIR
 OUT_FILE=$OUT_DIR/raw.txt
 
-pinpoint -c --timestamp -r 5 -i 250 -e rapl:pkg,GPU -o $OUT_FILE -- python nothing.py -o$OUT_DIR -t10
-chmod 0777 $OUT_FILE
+pinpoint -c --timestamp -r 5 -i 250 -e rapl:pkg,GPU -o $OUT_FILE -- python nothing.py -o$OUT_DIR -t12
+chmod a+w, a+r $OUT_FILE
