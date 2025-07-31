720692b89194eb0ed8836b81608b30b04334d581
FoyerD <amitfoye@post.bgu.ac.il>
2025-07-31 15:13:29 +0300

    cleaned up

--- scripts/get_exp.sh
+++ scripts/get_exp.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-if [ -n "$1" ]; then
+if [ $1 ]; then
 	scp -r -i ~/.ssh/id_amit -P 5050 debian@100.95.44.40:~/repos/Energy_Measurer/out_files/exp_$1 ~/repos/Energy_Measurer/out_files/exp_$1
+fi
+else
+	echo "Please provide the experiment name as an argument."
 fi
