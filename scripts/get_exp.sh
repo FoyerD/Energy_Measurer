20e9540891305da9502aa0ccc56e6877e4e6e1fa
FoyerD <amitfoye@post.bgu.ac.il>
2025-08-03 17:31:50 +0300

    changed measure scripts, added batch script

--- scripts/get_exp.sh
+++ scripts/get_exp.sh
@@ -3,6 +3,3 @@
 if [ $1 ]; then
 	scp -r -i ~/.ssh/id_amit -P 5050 debian@100.95.44.40:~/repos/Energy_Measurer/out_files/exp_$1 ~/repos/Energy_Measurer/out_files/exp_$1
 fi
-else
-	echo "Please provide the experiment name as an argument."
-fi
--- scripts/measure.sh
+++ scripts/measure.sh
@@ -5,9 +5,12 @@ export PATH="/home/debian/anaconda3/envs/EM/bin/:/home/debian/repos/pinpoint/bui
 OUT_DIR=""
 NUM_EXPS=1
 SETUP_FILE="setups/setup.toml"
+DOMAIN=""
+CROSSOVER_OP=""
+MUTATION_OP=""
 
 
-while getopts "o:n:s:" opt; do
+while getopts "o:n:s:c:m:d:" opt; do
   case "$opt" in
     o)
         OUT_DIR="out_files/exp_$OPTARG"
@@ -20,11 +23,36 @@ while getopts "o:n:s:" opt; do
     s)
         SETUP_FILE=$OPTARG
         ;;
+    c)
+        CROSSOVER_OP=$OPTARG
+        ;;
+    m)
+        MUTATION_OP=$OPTARG
+        ;;
+    d)
+        DOMAIN=$OPTARG
+        ;;
   esac
 done
 
 if [ -z "$OUT_DIR" ]; then
         echo "Output directory not specified. Use -o <output_directory>."
+        exit 1
+fi
+if [ -z "$SETUP_FILE" ]; then
+        echo "Setup file not specified. Use -s <setup_file>."
+        exit 1
+fi
+if [ -z "$CROSSOVER_OP" ]; then
+        echo "Crossover operator not specified. Use -c <crossover_operator>."
+        exit 1
+fi
+if [ -z "$MUTATION_OP" ]; then
+        echo "Mutation operator not specified. Use -m <mutation_operator>."
+        exit 1
+fi
+if [ -z "$DOMAIN" ]; then
+        echo "Domain not specified. Use -d <domain>."
         exit 1
 fi
 
