f7aa693e47a6f300e880666f294258ac4ce04311
FoyerD <amitfoye@post.bgu.ac.il>
2025-10-20 12:19:25 +0000

    changed order of copying/moving setup file

--- scripts/batch_measure.sh
+++ scripts/batch_measure.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 for setup_file in setups/batch_setups/*.toml; do
-	echo $setup_file
-	scripts/measure.sh -n 20 -s "$setup_file" -o out_files -r
+	echo "---------$setup_file----------"
+	scripts/measure.sh -n 5 -s "$setup_file" -o out_files -r
 done
 
