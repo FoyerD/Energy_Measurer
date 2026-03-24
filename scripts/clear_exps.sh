2251f79eb4da0b828906734849caff9f6048bf06
FoyerD <amitfoye@post.bgu.ac.il>
2026-03-24 15:53:38 +0200

    fixed clear exps

--- scripts/clear_exps.sh
+++ scripts/clear_exps.sh
@@ -4,7 +4,7 @@ set +e
 for exp in "$@"; do
 
   rm -rf "$exp/imgs"
-  rm -f "$exp/mean_*.csv"
-  rm -rf "$exp/parsed_*/"
+  rm -f "$exp"/mean_*.csv
+  rm -rf "$exp"/parsed_*/
   echo "done: $exp"
 done
