d250e54890a159e2f2a4f6d23d7690e2b7205a6b
FoyerD <amitfoye@post.bgu.ac.il>
2026-04-06 14:12:43 +0300

    changed gc to higher-is-better

--- exp_runner.py
+++ exp_runner.py
@@ -43,7 +43,7 @@ def main(output_dir:str, setup_file:str=None):
     elif(domain_name == 'graph_coloring'):
         evaluator, individual_length, min_bound, max_bound = EckityFactory.make_gc_evaluator(**config['domain']['args'])
         creator = GAIntegerStringVectorCreator(length=individual_length, bounds=(min_bound, max_bound))
-        higher_is_better = False
+        higher_is_better = True
 
     else:
         raise ValueError(f'Domain {domain_name} not recognized')
