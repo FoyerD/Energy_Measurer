c10ab6a17b163c59c10e61f83ff3f21c1e3df10f
FoyerD <amitfoye@post.bgu.ac.il>
2026-06-06 14:40:30 +0300

    removed pkg and gpu from plot

--- plot_scripts/plot.py
+++ plot_scripts/plot.py
@@ -106,21 +106,21 @@ def plot_statistics_over_time(measures_df, statistics_df, output_dir:str, marker
     axes[0].tick_params(axis='both', labelsize=12)
     axes[1].tick_params(axis='both', labelsize=12)
 
-    axes[0].plot(measures_df['time'], measures_df['PKG'], color='red', label='PKG MJ')
-    axes[0].fill_between(
-        measures_df['time'],
-        measures_df['PKG'] - measures_df['PKG_std'],
-        measures_df['PKG'] + measures_df['PKG_std'],
-        color='red', alpha=0.2
-    )
+    # axes[0].plot(measures_df['time'], measures_df['PKG'], color='red', label='PKG MJ')
+    # axes[0].fill_between(
+    #     measures_df['time'],
+    #     measures_df['PKG'] - measures_df['PKG_std'],
+    #     measures_df['PKG'] + measures_df['PKG_std'],
+    #     color='red', alpha=0.2
+    # )
 
-    axes[0].plot(measures_df['time'], measures_df['GPU'], color='blue', label='GPU MJ')
-    axes[0].fill_between(
-        measures_df['time'],
-        measures_df['GPU'] - measures_df['GPU_std'],
-        measures_df['GPU'] + measures_df['GPU_std'],
-        color='blue', alpha=0.2
-    )
+    # axes[0].plot(measures_df['time'], measures_df['GPU'], color='blue', label='GPU MJ')
+    # axes[0].fill_between(
+    #     measures_df['time'],
+    #     measures_df['GPU'] - measures_df['GPU_std'],
+    #     measures_df['GPU'] + measures_df['GPU_std'],
+    #     color='blue', alpha=0.2
+    # )
     
     
     axes[0].plot(measures_df['time'], measures_df['TOTAL'], color='purple', label='Total MJ')
